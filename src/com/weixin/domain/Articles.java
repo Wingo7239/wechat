64f6a7071d81931566908ff98119f853b6f90ca7
guojiangshan <guojiangshan@gmail.com>
2013-10-05 17:29:05 +0800

    Revert "rollback by jonsan"
    This reverts commit 7544125d24315b99b5fc6a0a2dc4cd188314bb57.

---
+++ src/com/weixin/domain/Articles.java
@@ -0,0 +1,16 @@
+package com.weixin.domain;
+
+public class Articles {
+
+    private Item item;
+
+    public Item getItem() {
+        return item;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
+    }
+    
+    
+}
